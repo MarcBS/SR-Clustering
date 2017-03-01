ca5a5dce56d0590905efcc9be787e4c7bb26b2a6
Seidor <marc@valdivia.cat>
2017-03-01 10:59:27 +0100

    runRequestTaggingYOLO almost done

--- Concept_Detector/runRequestTaggingYOLO.py
+++ Concept_Detector/runRequestTaggingYOLO.py
@@ -1,4 +1,5 @@
-
+import os
+import ntpath
 
 def main():
 
@@ -9,3 +10,31 @@ def main():
 
 	# Result parameters
 	path_result = '%result_path%'
+
+    ## Apply on each image
+	nFolders = len(folders)
+	for f in range(nFolders):
+
+	    # Create results directory
+	    result_dir = path_result + '/' + folders[f]
+	    if not os.path.exists(result_dir):
+	        os.makedirs(result_dir)
+
+        list_imgs = glob(path_images + '/' + folders[f] + '/*' + formats[f])
+
+        # TODO: Obtain  data from bash
+
+        for i in range(len(list_imgs)):
+
+            img_name = ntpath.basename(list_imgs[i])
+
+    		if not os.path.isfile(result_dir + '/' + img_name + '.json'):
+
+    			# Store data
+    			text_file = open(result_dir + '/' + img_name + '.json', "w")
+    			text_file.write("%s" % tags)
+    			text_file.close()
+
+	    print "Processed folder " + folders[f]
+
+	print "Done"
